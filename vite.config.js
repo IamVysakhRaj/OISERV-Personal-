13a0c55af5709b8901f783eaa370e38ccfe006b3
Vysakh Raj <Vysakh.Raj@Orioninc.com>
2024-10-15 11:54:15 +0530

    fixing vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '', 
   //base: './',  // Correct base URL for GitHub Pages deployment
   build: {
     rollupOptions: {
